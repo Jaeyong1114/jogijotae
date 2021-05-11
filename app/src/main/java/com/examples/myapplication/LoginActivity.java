b22be98da73206729b3312be7ce0f010a8cb70ea
Jaeyong1114 <ajfnwodyd3@naver.com>
2021-05-11 22:37:06 +0900

    손현호 테스트

--- app/src/main/java/com/examples/myapplication/LoginActivity.java
+++ app/src/main/java/com/examples/myapplication/LoginActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
-
+//손현호
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
