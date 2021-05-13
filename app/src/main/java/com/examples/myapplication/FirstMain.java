d3ab22d4b200adcadb4449efeb3eca38099a3048
Jaeyong1114 <ajfnwodyd3@naver.com>
2021-05-13 19:49:40 +0900

    하씨팔 좆같네

---
+++ app/src/main/java/com/examples/myapplication/FirstMain.java
@@ -0,0 +1,38 @@
+package com.examples.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstMain extends AppCompatActivity implements View.OnClickListener {
+    Button btn01,btn02;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_main);
+    btn01=findViewById(R.id.button2);
+    btn02=findViewById(R.id.button);
+    btn01.setOnClickListener(this);
+    btn02.setOnClickListener(this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()== R.id.button2) {
+            Intent intent01 = new Intent(this, LoginActivity.class);
+            startActivity(intent01);
+
+        }
+        if(v.getId()==R.id.button){
+
+        }
+    }
+}
