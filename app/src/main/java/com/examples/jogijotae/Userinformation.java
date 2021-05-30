0d8c5d5d66763360c21d67d40edd354c16fbe2b2
Jaeyong1114 <ajfnwodyd3@naver.com>
2021-05-30 22:28:56 +0900

    성별 체크박스

--- app/src/main/java/com/examples/jogijotae/Userinformation.java
+++ app/src/main/java/com/examples/jogijotae/Userinformation.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -21,6 +23,9 @@ public class Userinformation extends AppCompatActivity {
     //다시수정한다 씨팔
     private static final String TAG = "Memberinfo";
     private FirebaseAuth mAuth;
+    private RadioGroup rg_gender;
+    private RadioButton rb_man,rb_woman;
+    private String str_result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +33,29 @@ public class Userinformation extends AppCompatActivity {
         setContentView(R.layout.userinformation);
 
         mAuth = FirebaseAuth.getInstance();
+        rg_gender=findViewById(R.id.rg_gender); //라디오 버튼들을 담고있는 그룹
+        rb_man=findViewById(R.id.rb_man); // 라디오 버튼
+        rb_woman = findViewById(R.id.rb_woman); // 라디오 버튼
 
         findViewById(R.id.checkButton).setOnClickListener(onClickListener);
+
+
+        rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                if(i == R.id.rb_man) {
+                    str_result = rb_man.getText().toString();
+                }
+                else if(i == R.id.rb_woman){
+                    str_result=rb_woman.getText().toString();
+                }
+
+            }
+        });
     }
+
+
+
 
     View.OnClickListener onClickListener = (v) -> {
         switch (v.getId()) {
@@ -43,10 +68,11 @@ public class Userinformation extends AppCompatActivity {
     private void profileUpdate() {
         String birthyear = ((EditText) findViewById(R.id.birth_editText)).getText().toString();
         String phone = ((EditText) findViewById(R.id.phone_editText)).getText().toString();
-        String gender= ((EditText) findViewById(R.id.gender_editText)).getText().toString();
+        String gender= str_result;
         String name = ((EditText) findViewById(R.id.name_editText)).getText().toString();
 
-        if (birthyear.length() >3 && birthyear.length() <5 && phone.length() > 12 ) {
+
+        if (birthyear.length() >3 && birthyear.length() <5 && phone.length() > 12 && name.length()>0 ) {
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
             Memberinfo memberInfo = new Memberinfo(birthyear,phone,gender,name);
